78e5aed4085b27124544670cf523e62a2fbbc058
JosephCarmeli <josephcarmeli@hotmail.com>
2023-10-28 20:05:09 -0500

    adding style to error pages

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto p-4">
+        <h1 class="text-xl font-bold text-red-500">Error 400</h1>
+        <p class="text-gray-700">Bad Request</p>
+        <p class="mt-4">The request could not be understood by the server due to malformed syntax.</p>
+    </div>
+@endsection
