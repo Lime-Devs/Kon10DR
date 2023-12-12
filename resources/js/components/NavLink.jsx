742216ec7a43609ec729823e2143d0f72bb6ee17
Jose de Lima <delimajm@gmail.com>
2023-12-11 22:08:47 -0600

    fixed navbar font styles

--- resources/js/components/NavLink.jsx
+++ resources/js/components/NavLink.jsx
@@ -2,13 +2,13 @@ import { Link } from '@inertiajs/react';
 
 export default function NavLink({ active = false, className = '', children, ...props }) {
     return (
-        <Link
+        <a
             {...props}
             className={
-                'inline-flex items-center font-bangers font-sans text-2xl p-6 text-white hover:text-orange-500 font-medium leading-5 transition duration-150 ease-in-out focus:outline-none '
+                'inline-flex items-center font-bangers text-2xl p-6 text-white hover:text-orange-500 font-medium leading-5 transition duration-150 ease-in-out focus:outline-none'
             }
         >
             {children}
-        </Link>
+        </a>
     );
 }
